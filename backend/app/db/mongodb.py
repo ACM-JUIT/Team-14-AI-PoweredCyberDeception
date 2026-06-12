d3103da631c249f9e903283bf58efc03b005f9da
Namish Sharma <namishsharma@Namishs-MacBook-Air.local>
2026-06-13 01:59:15 +0530

    setup backend project skeleton

---
+++ backend/app/db/mongodb.py
@@ -0,0 +1,23 @@
+from motor.motor_asyncio import AsyncIOMotorClient
+from app.core.config import settings
+
+client = None
+database = None
+
+
+async def connect_to_mongo():
+    global client, database
+
+    client = AsyncIOMotorClient(settings.MONGO_URI)
+
+    print("MongoDB connected")
+
+
+async def close_mongo_connection():
+    global client
+
+    if client:
+        client.close()
+
+    print("MongoDB disconnected")
+
